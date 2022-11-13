012daa03762a064df85fce8c9c387634cb10a031
Dominik Baranski <dbaranski@edu.cdv.pl>
2022-11-13 15:37:21 +0100

    dodanie plikow zrobionych w innym repo

---
+++ data.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "data.h"
+#include "kontrola_liczb.h"
+#include <string>
+
+
+using namespace std;
+
+string podaj_imie() {
+	string imie;
+	cout << "Podaj swoje imie: ";
+	cin >> imie;
+	return imie;
+};
+bool podaj_miejsce()
+{
+	string odp;
+	cout << "Czy chcesz zlozyc zamowienie na miejscu czy z dowozem?" << endl;
+	while (true) {
+		cout << "M - na miejscu, D - z dowozem: ";
+		cin >> odp;
+		if (odp == "M" || odp == "m")
+		{
+			return true;
+		}
+		else if (odp == "D" || odp == "d")
+		{
+			return false;
+		}
+		else
+		{
+			cout << "Podano bledna odpowiedz!" << endl;
+		}
+	}
+};
+int nr_stolika() {
+	int nr;
+	bool blad;
+	do
+	{
+		blad = false;
+		cout << "Podaj nr stolika ktory chcesz zajac (1-50)"<<endl;
+		nr = kontrola_liczby();
+		if (nr > 50 || nr < 1)
+		{
+			cout << "Bledny numer stolika"<<endl;
+			blad = true;
+		}
+	} 
+	while (blad);
+	
+	return nr;
+};
+void podaj_adres(string & miejscowosc, string & ulica, int & nr_domu, int & nr_mieszkania)
+{
+	cout << "Podaj miejscowosc: ";
+	cin >> miejscowosc;
+	cout << "ulica: ";
+	cin >> ulica;
+	cout << "numer domu: ";
+	nr_domu = kontrola_liczby();
+	cout << "numer mieszkania : ";
+	nr_mieszkania = kontrola_liczby();
+};
---
+++ data.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+string podaj_imie();
+
+bool podaj_miejsce();
+
+int nr_stolika();
+
+void podaj_adres(string &, string &, int &, int &);
+
+void wybierz_godzine_dostawy(int);
