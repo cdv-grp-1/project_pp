571d6d3df3bd94db26f4988edb412f9e91403442
Dominik Baranski <baranskidominik109@gmail.com>
2022-12-01 03:55:44 +0100

    merge

--- menu.h
+++ menu.h
@@ -8,10 +8,5 @@ using namespace std;
 
 void menu_lista();
 string menu_nazwa(int);
-<<<<<<< HEAD
-void menu_dodaj(int [20][2]);
 double menu_cena(int);
-=======
-string menu_cena(int);
 void menu_dodaj(int [20][2]);
->>>>>>> b9e117188a1257fd910b318a6cf626250e0d9441
